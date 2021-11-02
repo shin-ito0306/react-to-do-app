e5a7515018804a74b29da688bdbc7407f6f72609
shin <65806493+shin-ito0306@users.noreply.github.com>
2021-11-02 12:14:50 +0900

    add CSS

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,40 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="input-area">
+        <input placeholder="TODOの入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          <div className="list-row">
+            <li>あああ</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+          <div className="list-row">
+            <li>いいい</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
+        <ul>
+          <div className="list-row">
+            <li>ううう</li>
+            <button>戻す</button>
+          </div>
+          <div className="list-row">
+            <li>えええ</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
